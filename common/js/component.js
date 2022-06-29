3d9eff9f56fa982a67f60dedaaac2a1210b2f7de
jm011jh <jm011jh@gmail.com>
2022-06-29 18:28:52 +0900

    nav scroll open

--- common/js/component.js
+++ common/js/component.js
@@ -186,3 +186,19 @@ function navSwitching(){
     }
 
 }
+
+var lastScrollTop = 0, delta = 15;
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop() /* 스크롤바 수직 위치를 가져옵니다, 괄호 안에 값(value)이 있을 경우 스크롤바의 수직 위치를 정합니다. */
+    // Math.abs: 주어진 숫자의 절대값을 반환(return)합니다.
+    if(Math.abs(lastScrollTop - scrollTop) <= delta) // 스크롤 값을 받아서 ~
+    return; // ~ 리턴
+
+    if ((scrollTop > lastScrollTop) && (lastScrollTop>0)) {
+        $("custom-nav").removeClass("is-show");
+        $("custom-nav").removeClass("is-open");
+    } else {
+        $("custom-nav").addClass("is-show");
+    }
+    lastScrollTop = scrollTop;
+});
